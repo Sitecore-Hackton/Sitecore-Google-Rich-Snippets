51cee358da50ec7cdab91fa4bdbadb7003f268c5
Vipin Banka <vbanka@rightpoint.com>
2017-03-04 10:18:11 +0530

    Initial commit

---
+++ Sitecore.Google.Structure.Data/Sitecore.Google.Structure.Data/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Google.Structure.Data
+{
+    public static class Constants
+    {
+        public static class Fields
+        {
+            
+        }
+
+        public static class Templates
+        {
+            
+        }
+    }
+}
